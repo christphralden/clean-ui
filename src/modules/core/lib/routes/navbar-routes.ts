ad81c83570b631fccf92063fc9a423eaced97785
christopher-alden <christopheralden04@gmail.com>
2024-07-22 02:24:23 +0700

    feat: project structure and home layouting

---
+++ src/modules/core/lib/routes/navbar-routes.ts
@@ -0,0 +1,20 @@
+const NavbarRoutes = [
+    {
+        name:"Home",
+        route:["/"]
+    },
+    {
+        name:"Components",
+        route:["/components"]
+    },
+    {
+        name:"Sites",
+        route:["/sites"]
+    },
+    {
+        name:"More",
+        route:["/more"]
+    }
+]
+
+export {NavbarRoutes}
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'restart': ['Restart', 'sans-serif'],
+				'jha': ['Jha', 'serif'],
+			},
+			colors: {
+				'primary': '#4169E1',
+				'background': '#F9F6EE',
+				'accent': '#0000FF'
+			}
+		},
+	},
+	plugins: [],
+}
